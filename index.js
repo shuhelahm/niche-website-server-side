a3a8f318d42887446022cf4f95dc0eea9044a6b1
shuhelahm <shuhel037@gmail.com>
2021-11-12 22:09:40 +0600

    updated

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // pass: QoMaUNcE969EUAoD
 // name: car-bd
 
-const uri = "mongodb+srv://car-bd:91yNpTR75TqzIad7@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://car-bd:C9qWFC4tMslB6FrK@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run(){
   try{
       await client.connect();
       const database = client.db('car-bd');
       const productsCollection = database.collection('products');
+      console.log('database connected')
       // get api
       app.get('/products', async(req,res)=>{
         const cursor = productsCollection.find({});
         const products = await cursor.toArray();
         res.send(products)
       })
-      const doc = {
-        name: 'amiiii'
-      }
-      const result = await productsCollection.insertOne(doc);
-      console.log(result)
+      // const doc = {
+      //   name: 'amiiii'
+      // }
+      // const result = await productsCollection.insertOne(doc);
+
       // perform actions on the collection object
       // client.close();
+      // post api
     }finally{
       // await client.close();
   }
