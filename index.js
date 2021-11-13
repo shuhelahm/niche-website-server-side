a9be7570ad069925626baa6ba6262457a855a5a6
shuhelahm <shuhel037@gmail.com>
2021-11-13 23:26:20 +0600

    updated\

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const { query, application } = require('express');
 const port = process.env.PORT || 5000;
-
-// pass: QoMaUNcE969EUAoD
-// name: car-bd
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://car-bd:C9qWFC4tMslB6FrK@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run(){
   try{
       await client.connect();
       const database = client.db('car-bd');
       const productsCollection = database.collection('products');
+      const reviewsCollection = database.collection('reviews');
       console.log('database connected')
       // get api
       app.get('/products', async(req,res)=>{
@@ -25,10 +26,33 @@ async function run(){
         const products = await cursor.toArray();
         res.send(products)
       })
+      // get api reviews
+      app.get('/reviews', async(req,res)=>{
+        const cursor = reviewsCollection.find({});
+        const reviews = await cursor.toArray();
+        res.send(reviews)
+      })
+
+      app.get('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const product = await productsCollection.findOne(query);
+
+        res.send(product);
+      })
+
+      // post api
+      app.post('/reviews', async(req, res)=>{
+        const newReview = req.body;
+          console.log('hit the post', req.body)
+          const result = await reviewsCollection.insertOne(newReview);
+          res.json(result);
+      })
+
       // const doc = {
-      //   img:'https://4wheelerbd.com/wp-content/uploads/2020/10/nissan-petrol.jpg',name:'Nissan Patrol',price: 78000, description: 'The Nissan Patrol is an off-road large family 4×4 SUV. It offers a level of luxury and value. The Nissan Patrol is a series of four-wheel-drive vehicles manufactured by Nissan in Japan'
+      //   review:'good'
       // }
-      // const result = await productsCollection.insertOne(doc);
+      // const result = await reviewsCollection.insertOne(doc);
 
       // perform actions on the collection object
       // client.close();
